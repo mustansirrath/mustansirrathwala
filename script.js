e0dbddd689c3805373e37735ed10d99eccbd2a1d
Mustan Rathwala <rathmustan53@gmail.com>
2024-07-24 15:19:41 +0530

    new section add

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const openBtn = document.getElementById("menu-btn");
 const closeBtn = document.getElementById("menu-close");
+const themeWhite = document.getElementById('theme-white')
 const ul = document.getElementById("links");
 
 openBtn.addEventListener("click", showMenu);
 closeBtn.addEventListener("click", hideMenu);
+// themeWhite.addEventListener('click', () => {
+//   document.body.classList.add('bg-change-white')
+//   document.body.classList.remove('bg-change-black')
+//   themeWhite.classList.add('fa-regular fa-moon')
+//   themeWhite.classList.remove('fa-regular fa-sun')
+// })
 
 function showMenu() {
   ul.classList.add("active");
